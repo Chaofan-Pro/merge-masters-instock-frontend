1011e6d5aa46afd2653aeedd14b3ea5be8039daf
ChaofanMa <chaofanmah@gmail.com>
2025-02-04 14:27:54 -0500

    finish mobile version styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,25 @@
 import "./Header.scss";
+import { useLocation, Link } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation().pathname;
   return (
     <header className="header">
-      <div>
-        <img
-          className="header__logo"
-          src="/src/assets/logos/InStock-Logo 2.svg"
-          alt="instock logo"
-        />
-        <h1 className="header__title">INSTOCK</h1>
-      </div>
+      <img
+        className="header__logo"
+        src="/src/assets/logos/InStock-Logo.svg"
+        alt="instock logo"
+      />
       <nav className="header__nav-bar">
-        <ul>
-          <li className="header__nav-item">Warehouses</li>
-          <li className="header__nav-item">Invertory</li>
-        </ul>
+        <Link
+          to="/warehouses"
+          className={location ==="/warehouses"?"header__nav-item--active":"header__nav-item"}
+        >
+          Warehouses
+        </Link>
+        <Link to="/inventory" className={location ==="/inventory"?"header__nav-item--active":"header__nav-item"}>
+          Inventory
+        </Link>
       </nav>
     </header>
   );
