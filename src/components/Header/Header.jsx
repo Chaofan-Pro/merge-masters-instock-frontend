145b9a39807e2a8bd4d53d2b7942a888be262bfa
ChaofanMa <chaofanmah@gmail.com>
2025-02-04 14:54:03 -0500

    finish tablet and desktop version styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,19 +5,32 @@ const Header = () => {
   const location = useLocation().pathname;
   return (
     <header className="header">
-      <img
-        className="header__logo"
-        src="/src/assets/logos/InStock-Logo.svg"
-        alt="instock logo"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="/src/assets/logos/InStock-Logo.svg"
+          alt="instock logo"
+        />
+      </Link>
       <nav className="header__nav-bar">
         <Link
           to="/warehouses"
-          className={location ==="/warehouses"?"header__nav-item--active":"header__nav-item"}
+          className={
+            location === "/warehouses"
+              ? "header__nav-item--active"
+              : "header__nav-item"
+          }
         >
           Warehouses
         </Link>
-        <Link to="/inventory" className={location ==="/inventory"?"header__nav-item--active":"header__nav-item"}>
+        <Link
+          to="/inventory"
+          className={
+            location === "/inventory"
+              ? "header__nav-item--active"
+              : "header__nav-item"
+          }
+        >
           Inventory
         </Link>
       </nav>
