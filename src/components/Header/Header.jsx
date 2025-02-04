53ff2a8805c92e09dad90d092babab2c4c0b4f64
ChaofanMa <chaofanmah@gmail.com>
2025-02-04 13:45:53 -0500

    add basic header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
-import React from "react";
 import "./Header.scss";
 
 const Header = () => {
-  return <header>
-    <img src="" alt="" />
-  </header>;
+  return (
+    <header className="header">
+      <div>
+        <img
+          className="header__logo"
+          src="/src/assets/logos/InStock-Logo 2.svg"
+          alt="instock logo"
+        />
+        <h1 className="header__title">INSTOCK</h1>
+      </div>
+      <nav className="header__nav-bar">
+        <ul>
+          <li className="header__nav-item">Warehouses</li>
+          <li className="header__nav-item">Invertory</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
