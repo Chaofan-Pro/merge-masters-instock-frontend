38eac9d2fdcb74d95890f75720ad54f637e4e604
Sonal Sood <sonalsood97@gmail.com>
2025-02-04 17:54:29 -0500

    added styling for edit icon

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import backArrow from "../../assets/icons/arrow_back-24px.svg";
 import edit from "../../assets/icons/edit-white-24px.svg";
-import "./WarehouseDetails.scss";
 import axios from "axios";
+import "./WarehouseDetails.scss";
+
 const baseUrl = import.meta.env.VITE_APP_URL;
 
 const WarehouseDetails = () => {
+  const { id } = useParams(); // Get warehouse ID from URL
+  const navigate = useNavigate();
+  const [warehouse, setWarehouse] = useState(null);
+
+  useEffect(() => {
+    const fetchWarehouseDetail = async () => {
+      try {
+        const singleWarehouseRes = await axios.get(
+          baseUrl + `warehouses/${id}`
+        );
+        setWarehouse(singleWarehouseRes.data);
+      } catch (error) {
+        console.error("ERROR: " + error);
+      }
+    };
+    fetchWarehouseDetail();
+  }, [id]);
+
+  if (!warehouse) return <p>No Warehouse Detail Found</p>;
+
   return (
     <section className="warehouse-detail">
       <div className="warehouse-detail__heading-container">
@@ -13,7 +36,7 @@ const WarehouseDetails = () => {
           alt="Back Arrow Icon"
           className="warehouse-detail__back-icon"
         />
-        <h2 className="warehouse-detail__name">WAREHOUSE NAME</h2>
+        <h2 className="warehouse-detail__name">{warehouse.warehouse_name}</h2>
         <img
           src={edit}
           alt="Edit Icon"
