e52a7a5307ae0ec78640b84761cea5acc112a380
yoona oh <yoonaoh312@gmail.com>
2025-02-08 02:31:59 +0900

    forgot to checkout different branch

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,6 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
-        <CustomModal />
         <main>
           <Routes>
             {/* =-=-=-=-=-HOME PAGE-=-=-=-=-= */}
--- src/pages/WarehousesPage/WarehousesPage.jsx
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -1,10 +1,51 @@
 import "./WarehousesPage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import { useState } from "react";
+import CustomModal from "../../components/CustomModal/CustomModal";
 
 const WarehousePage = () => {
-  return (
+const[warehouses, setWarehouses] = useState([]);
+const[selectedWarehouse, setSelectedWarehouse] = useState(null);
+const[isModalOpen, setIsModalOpen] = useState(false);
+
+const fetchWarehouses = () => {
+  fetch(`${API_URL}/api/warehouses`)
+    .then((response) => response.json())
+    .then((data) => setWarehouses(data))
+    .catch((error) => console.error("Error getting warehouses:", error));
+};
+
+const openModal = (warehouse) => {
+  setSelectedWarehouse(warehouse);
+  setIsModalOpen(true);
+};
+
+const closeModal = () => {
+  setIsModalOpen(false);
+};
+
+const deleteWarehouse = (id) => {
+  fetch(`${API_URL}/api/warehouses/${id}`, {
+    method: "DELETE",
+  })
+    .then(() => fetchWarehouses()) 
+    .catch((error) => console.error("Error deleting warehouse:", error));
+};
+
+return (
     <>
-      <WarehouseList page={"warehouses"} />
+      <WarehouseList 
+      page={"warehouses"} 
+      openModal={openModal}
+      warehouses={warehouses}     
+      />
+      {selectedWarehouse && isModalOpen && (
+        <CustomModal
+          warehouse={selectedWarehouse}
+          closeModal={closeModal}
+          deleteWarehouse={deleteWarehouse}
+        />
+      )}
     </>
   );
 };
