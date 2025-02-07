89b67cf1345b42fbb5d0f65218cca343d898b07a
Sophia Tang <sophiatangst@Sophias-MacBook-Air.local>
2025-02-06 20:48:34 -0500

    update className for InventoryForm, AddInventoryFrom, jsx and scss

--- src/components/InventoryForm/InventoryForm.jsx
+++ src/components/InventoryForm/InventoryForm.jsx
@@ -3,38 +3,38 @@ import React from "react";
 
 function InventoryForm() {
   return (
-    <form className="Inventory__form">
+    <form className="inventory__form">
       {/* -=-=-=-=-=-=-=-ITEMS DETAILS CONTAINER-=-=-==-=-= */}
-      <div className="Inventory__details-container">
-        <h3 className="Inventory__subheader-title">Item Details</h3>
+      <div className="inventory__details-container">
+        <h3 className="inventory__subheader-title">Item Details</h3>
         {/* =-=-=-=-=-ITEM NAME-=-==-=- */}
-        <div className="Inventory__detail-form">
-          <label htmlFor="name" className="Inventory__detail-label">
+        <div className="inventory__detail-form">
+          <label htmlFor="name" className="inventory__detail-label">
             Item Name
           </label>
           <input
             type="text"
-            className="Inventory__detail-input"
+            className="inventory__detail-input"
             placeholder="Item Name"
             id="name"
             name="itemname"
           />
           {/* =-=-=-=-=-DESCRIPTION-=-==-=- */}
-          <label htmlFor="description" className="Inventory__detail-label">
+          <label htmlFor="description" className="inventory__detail-label">
             Description
           </label>
           <textarea
             type="text"
-            className="Inventory__detail-textarea"
+            className="inventory__detail-textarea"
             placeholder="Please enter a brief item description..."
             id="description"
             name="description"
           ></textarea>
           {/* =-=-=-=-=-CATEGORY-=-==-=- */}
-          <label htmlFor="category" className="Inventory__detail-label">
+          <label htmlFor="category" className="inventory__detail-label">
             Category
           </label>
-          <div className="Inventory__select-container">
+          <div className="inventory__select-container">
             <select name="category" id="category" className="category__select">
               <option value="Select">Please select</option>
               <option value="Electronics">Electronics</option>
@@ -48,11 +48,11 @@ function InventoryForm() {
       </div>
 
       {/* -=-=-=-=-=-=-=-ITEMS AVAILABILITY CONTAINER-=-=-==-=-= */}
-      <div className="Inventory__availability-container">
-        <div className="Inventory__availability-form">
-          <h3 className="Inventory__subheader-title">Item availability</h3>
+      <div className="inventory__availability-container">
+        <div className="inventory__availability-form">
+          <h3 className="inventory__subheader-title">Item availability</h3>
           {/* =-=-=-=-=-RADIO BUTTONS-=-==-=- */}
-          <label htmlFor="status" className="Inventory__detail-label">
+          <label htmlFor="status" className="inventory__detail-label">
             Status
           </label>
           <div className="stock__status">
@@ -78,12 +78,12 @@ function InventoryForm() {
             </label>
           </div>
           {/* =-=-=-=-=-QUANTITY-=-==-=- */}
-          <label htmlFor="quantity" className="Inventory__detail-label">
+          <label htmlFor="quantity" className="inventory__detail-label">
             Quantity
           </label>
           <input
             type="text"
-            className="Inventory__quantity"
+            className="inventory__quantity"
             placeholder="0"
             id="quantity"
             name="quantity"
@@ -91,10 +91,10 @@ function InventoryForm() {
         </div>
 
         {/* =-=-=-=-=-WAREHOUSE-=-==-=- */}
-        <label htmlFor="category" className="Inventory__detail-label">
+        <label htmlFor="category" className="inventory__detail-label">
           Warehouse
         </label>
-        <div className="Inventory__select-container">
+        <div className="inventory__select-container">
           <select name="warehouse" id="warehouse" className="warehouse__select">
             <option value="Select">Please select</option>
           </select>
