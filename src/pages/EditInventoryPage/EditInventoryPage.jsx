f67940e4e9b34aa6fdb8211c0cdd3060d0ee5242
Sophia Tang <sophiatangst@Sophias-MacBook-Air.local>
2025-02-06 20:41:59 -0500

    elements for editing inventory items, and styling

--- src/pages/EditInventoryPage/EditInventoryPage.jsx
+++ src/pages/EditInventoryPage/EditInventoryPage.jsx
@@ -1,11 +1,34 @@
 import "./EditInventoryPage.scss";
 import React from "react";
+import InventoryForm from "../../components/InventoryForm/InventoryForm";
 
 function EditInventory() {
-  return;
-  <>
-    <h3>This is the EditInventory page</h3>;
-  </>;
+  return (
+    <div className="editInventory">
+      <div className="editInventory__subheader-container">
+        <Link to="/inventory">
+          <img src={backArrow} alt="back arrow" className="back-arrow" />
+        </Link>
+        <h2 className="editInventory__title">Add New Inventory Item</h2>
+      </div>
+
+      <InventoryForm />
+
+      {/* =-=-=-=-=-=-BUTTONS-=-=-=-==- */}
+      <div className="buttons">
+        <Link to="/inventory" className="cancel__button">
+          Cancel
+        </Link>
+        <button
+          type="submit"
+          form="form"
+          className="save__button editInventory__button"
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default EditInventory;
