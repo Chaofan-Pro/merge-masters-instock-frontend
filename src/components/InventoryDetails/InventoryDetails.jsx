3ebe5c2d25012d258fa5f751c268502aa796a5d3
Sonal Sood <sonalsood97@gmail.com>
2025-02-07 21:11:18 -0500

    updated link to edit pages

--- src/components/InventoryDetails/InventoryDetails.jsx
+++ src/components/InventoryDetails/InventoryDetails.jsx
@@ -26,7 +26,7 @@ const InventoryDetails = ({ fetchInventoryDetail, inventory }) => {
           </Link>
           <h1 className="main-heading__name">{inventory.item_name}</h1>
         </div>
-        <Link to="/inventory/edit/:id">
+        <Link to={`/inventory/edit/${id}`}>
           <button className="main-heading__edit-button">
             <img src={edit} alt="Edit Icon" />
             <span className="main-heading__edit-text">Edit</span>
