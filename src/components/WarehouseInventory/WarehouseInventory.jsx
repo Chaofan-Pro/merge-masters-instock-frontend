2d87d6f54836c13e0e1f18a4159a00782f145c13
Sonal Sood <sonalsood97@gmail.com>
2025-02-08 09:38:51 -0500

    Added styling for mobile

--- src/components/WarehouseInventory/WarehouseInventory.jsx
+++ src/components/WarehouseInventory/WarehouseInventory.jsx
@@ -5,6 +5,7 @@ import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import rightArrowIcon from "../../assets/icons/chevron_right-24px.svg";
 import CustomModal from "../CustomModal/CustomModal";
 import axios from "axios";
+import React from "react";
 import "./WarehouseInventory.scss";
 
 const baseUrl = import.meta.env.VITE_API_URL;
@@ -29,7 +30,7 @@ const WarehouseInventory = () => {
 
   if (!warehouseInventory) return <p>No Warehouse Inventory Details Found</p>;
   return (
-    <section className="inventory-details">
+    <section className="warehouse-inventory-details">
       <div className="inventory-heading-container">
         <h4 className="inventory-heading__text">INVENTORY ITEM</h4>
         <h4 className="inventory-heading__text">CATEGORY</h4>
@@ -40,40 +41,94 @@ const WarehouseInventory = () => {
         </h4>
       </div>
       {warehouseInventory.map((item, index) => (
-        <div key={index} className="inventory-item">
-          <Link to={`/inventory/${id}`}>
-            <h4 className="inventory-item__text inventory-item__text--indigo">
-              {item.item_name}
-              <img
-                src={rightArrowIcon}
-                alt="Go to inventory"
-                className="inventory-item__arrow-icon"
-              />
-            </h4>
-          </Link>
-          <p className="inventory-item__text">{item.category}</p>
-          <div className="inventory-item__status-tag">
-            <h4
-              className={`${
-                item.status === "In Stock" ? "status-green" : "status-red"
-              }`}
-            >
-              {item.status.toUpperCase()}
-            </h4>
-          </div>
-          <p className="inventory-item__text">{item.quantity}</p>
-          <div className="inventory-item__actions">
-            {/* Link to Modal */}
-            <button className="inventory-item__action-icon">
-              <img src={deleteIcon} alt="Delete" />
-            </button>
-            <Link to={`/inventory/edit/${id}`}>
-              <button className="inventory-item__action-icon">
-                <img src={editIcon} alt="Edit" />
+        <React.Fragment key={index}>
+          {/* Warehouse Inventory List for Mobile */}
+          <div key={index} className="single-inventory-item">
+            <div className="single-inventory-item--row">
+              <div className="single-inventory-item__detail">
+                <h4 className="inventory-heading__text">INVENTORY ITEM</h4>
+                <Link to={`/inventory/${item.id}`}>
+                  <h4 className="inventory-item__text inventory-item__text--indigo">
+                    {item.item_name}
+                    <img
+                      src={rightArrowIcon}
+                      alt="Go to inventory"
+                      className="inventory-item__arrow-icon"
+                    />
+                  </h4>
+                </Link>
+              </div>
+              <div className="single-inventory-item__detail">
+                <h4 className="inventory-heading__text">STATUS</h4>
+                <div className="inventory-item__status-tag">
+                  <h4
+                    className={`${
+                      item.status === "In Stock" ? "status-green" : "status-red"
+                    }`}
+                  >
+                    {item.status.toUpperCase()}
+                  </h4>
+                </div>
+              </div>
+            </div>
+            <div className="single-inventory-item--row">
+              <div className="single-inventory-item__detail">
+                <h4 className="inventory-heading__text">CATEGORY</h4>
+                <p className="inventory-item__text">{item.category}</p>
+              </div>
+              <div className="single-inventory-item__detail">
+                <h4 className="inventory-heading__text">QTY</h4>
+                <p className="inventory-item__text">{item.quantity}</p>
+              </div>
+            </div>
+            <div className="single-inventory-item--row">
+              {/* Link to Delete Modal */}
+              <button className="single-inventory-item__action-icon">
+                <img src={deleteIcon} alt="Delete" />
               </button>
-            </Link>
+              <Link to={`/inventory/edit/${item.id}`}>
+                <button className="single-inventory-item__action-icon">
+                  <img src={editIcon} alt="Edit" />
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
+          {/* Warehouse Inventory List for tablet and desktop */}
+          <div className="inventory-item">
+            <Link to={`/inventory/${item.id}`}>
+              <h4 className="inventory-item__text inventory-item__text--indigo">
+                {item.item_name}
+                <img
+                  src={rightArrowIcon}
+                  alt="Go to inventory"
+                  className="inventory-item__arrow-icon"
+                />
+              </h4>
+            </Link>
+            <p className="inventory-item__text">{item.category}</p>
+            <div className="inventory-item__status-tag">
+              <h4
+                className={`${
+                  item.status === "In Stock" ? "status-green" : "status-red"
+                }`}
+              >
+                {item.status.toUpperCase()}
+              </h4>
+            </div>
+            <p className="inventory-item__text">{item.quantity}</p>
+            <div className="inventory-item__actions">
+              {/* Link to Modal */}
+              <button className="inventory-item__action-icon">
+                <img src={deleteIcon} alt="Delete" />
+              </button>
+              <Link to={`/inventory/edit/${item.id}`}>
+                <button className="inventory-item__action-icon">
+                  <img src={editIcon} alt="Edit" />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </React.Fragment>
       ))}
     </section>
   );
