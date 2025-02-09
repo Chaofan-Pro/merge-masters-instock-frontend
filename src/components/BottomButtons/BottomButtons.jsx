7e4fd2101b01c1e63eda2f71252dbf42e9774f2b
Sophia Tang <tang.sophia.st@gmail.com>
2025-02-09 12:01:03 -0500

    modify BottomButtons to include onClick and add back img, removed button styling from addInventoryPage and editInventoryPage

--- src/components/BottomButtons/BottomButtons.jsx
+++ src/components/BottomButtons/BottomButtons.jsx
@@ -1,17 +1,17 @@
 import "./BottomButtons.scss";
 import { Link } from "react-router-dom";
 
-const BottomButtons = ({ link, text, onClick }) => {
+const BottomButtons = ({ link, img, text, onClick }) => {
   return (
     <section className="button__container">
       <Link to={link} className="button__cancel">
         Cancel
       </Link>
-      <button type="button" className="button__save" onClick={onClick}>
+      <button type="submit" className="button__save" onClick={onClick}>
+        <img src={img} alt="" />
         {text}
       </button>
     </section>
   );
 };
-
 export default BottomButtons;
